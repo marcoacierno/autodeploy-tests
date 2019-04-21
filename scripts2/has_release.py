9cb76e01a307e043aec20f196639a55b3feac49d
Marco Acierno <marcoaciernoemail@gmail.com>
2019-04-21 12:39:05 +0200

    message

--- scripts2/has_release.py
+++ scripts2/has_release.py
@@ -10,6 +10,7 @@ from base import run_process, RELEASE_FILE, CHANGELOG_FILE
 
 if __name__ == '__main__':
     if not os.path.exists(RELEASE_FILE):
+        print("Not releasing a new version because there isn't a RELEASE.md file.")
         run_process([
             'circleci', 'step', 'halt'
         ])
